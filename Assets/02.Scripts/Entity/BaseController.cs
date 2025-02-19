f81aeaa2650bc6eb0908cc5b9a8e650f4a7728b5
hgkim0728 <hgkim0728@gmail.com>
2025-02-19 16:24:58 +0900

    원거리 공격 구현

--- Assets/02.Scripts/Entity/BaseController.cs
+++ Assets/02.Scripts/Entity/BaseController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,9 +8,11 @@ public class BaseController : MonoBehaviour
     protected Rigidbody2D rigid;
     protected AnimationHandler animationHandler;
     protected StatHandler statHandler;
+    protected WeaponHandler weaponHandler;
 
     [SerializeField] private SpriteRenderer characterRenderer;
     [SerializeReference] private Transform weaponPivot;
+    [SerializeField] private WeaponHandler weaponPrefab;
 
     protected Vector2 movementDirection = Vector2.zero;
     public Vector2 MovementDirection { get { return movementDirection; } }
@@ -20,11 +23,19 @@ public class BaseController : MonoBehaviour
     private Vector2 knockback = Vector2.zero;
     private float knockbackDuration = 0.0f;
 
+    protected bool isAttacking;
+    private float timeSinceLastAttack = float.MaxValue;
+
     protected virtual void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         animationHandler = GetComponent<AnimationHandler>();
         statHandler = GetComponent<StatHandler>();
+
+        if (weaponPrefab != null)
+            weaponHandler = Instantiate(weaponPrefab, weaponPivot);
+        else
+            weaponHandler = GetComponentInChildren<WeaponHandler>();
     }
 
     protected virtual void Start()
@@ -36,6 +47,7 @@ public class BaseController : MonoBehaviour
     {
         HandleAction();
         Rotate(lookDirection);
+        HandleAttackDelay();
     }
 
     protected virtual void FixedUpdate()
@@ -78,11 +90,36 @@ public class BaseController : MonoBehaviour
         {
             weaponPivot.rotation = Quaternion.Euler(0f, 0f, rotZ);
         }
+
+        weaponHandler?.Rotate(isLeft);
     }
 
     public void ApplyKnockback(Transform other, float power, float duration)
     {
         knockbackDuration = duration;
         knockback = -(other.position - transform.position).normalized * power;
+    }
+
+    private void HandleAttackDelay()
+    {
+        if (weaponHandler == null)
+            return;
+
+        if(timeSinceLastAttack <= weaponHandler.Delay)
+        {
+            timeSinceLastAttack += Time.deltaTime;
+        }
+
+        if(isAttacking && timeSinceLastAttack > weaponHandler.Delay)
+        {
+            timeSinceLastAttack = 0;
+            Attack();
+        }
+    }
+
+    protected void Attack()
+    {
+        if (lookDirection != Vector2.zero)
+            weaponHandler?.Attack();
     }
 }
